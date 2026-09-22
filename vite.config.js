d158470b3b850433ee8d09e4469b83ba044f703a
trung8356621 <trungtien1993@gmail.com>
2026-09-22 13:57:47 +0700

    0.5.8.28

--- vite.config.js
+++ vite.config.js
@@ -33,6 +33,7 @@ export default defineConfig({
                 'addons/seo/resources/js/keyword-destinations-modal.jsx',
                 'addons/seo/resources/js/domain-context.js',
                 'addons/seo/resources/css/operational-landing-dashboard.css',
+                'addons/seo/resources/css/ops-statistics.css',
                 'addons/media/resources/css/media-library.css',
                 'addons/media/resources/css/image-splitter.css',
                 'addons/media/resources/js/media-library-actions.js',
